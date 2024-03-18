fa7467e0d9866b127ed963be907265d6fdab9b54
VadimRuslanch <Merzlyy.Pes@yandex.ru>
2024-03-18 23:09:52 +0300

    mod: added photoGallerySlider

--- src/components/common/Main/ElectronicPressKit/infoPressKit.tsx
+++ src/components/common/Main/ElectronicPressKit/infoPressKit.tsx
@@ -1,12 +1,16 @@
 import styles from './infoPressKit.module.sass'
+
 import EventCard from "@/components/common/Main/ElectronicPressKit/EventCard/eventCard";
-import photo_1 from '@/assets/images/photo-1.png'
+import React, {useState} from "react";
+import {photoGallery} from "@/components/utils/constants";
+import PhotoSlideComponent from "@/components/common/Main/Intro/NewSingle/PhotoSlider/photoSlider";
 import Image from "next/image";
 import Link from "next/link";
 
 import arrowLink from '@/assets/images/arrow-link.svg'
 
 function ElectronicPressKitComponent() {
+
     return (
         <section className={styles.infoPressContainer}>
 
@@ -14,16 +18,19 @@ function ElectronicPressKitComponent() {
 
             <div className={styles.infoContainer}>
                 <h3 className={styles.titleBlock}>О нас</h3>
-                <p className={styles.aboutBlock}></p>
+                <p className={styles.aboutBlock}>
+                    Cover-trio SONX создаст в Вашем заведении романтичную и теплую атмосферу для гостей. Выступаем на
+                    мероприятиях: корпоративах, праздниках, днях рождения, свадебных церемониях. Создаем негромкий фон
+                    на бизнес встречах, конференциях, ресторанах, ТЦ. В концертной программе современный соул, R&B,
+                    зарубежные кавер-версии хитов в стиле лаундж, а также танцевальные хиты.
+                    <br/>
+                    Ростов-на-Дону, рассматриваем также предложения с выездом за город.
+                </p>
             </div>
 
             <div className={styles.infoContainer}>
                 <h3 className={styles.titleBlock}>Фотогалерея</h3>
-                <div className={styles.slider}>
-                    <div className={styles.sliderWr}>
-                        <div className={styles.slide}></div>
-                    </div>
-                </div>
+                <PhotoSlideComponent/>
             </div>
 
             <div className={styles.infoContainer}>
@@ -40,8 +47,12 @@ function ElectronicPressKitComponent() {
                 <div className={styles.eventList}>
 
                     <div className={styles.eventItem}>
-                        <h4 className={styles.eventTitle}>Мой концерт</h4>
-                        <span className={styles.eventDate}>Дата мероприятия</span>
+                        <h4 className={styles.eventTitle}>Tangiers Lounge</h4>
+                        <span className={styles.eventDate}>
+                            12 апреля 2024 г.
+                            <br/>
+                            г. Ростов-на-Дону, ул. Пушкинская, дом 116
+                        </span>
                         <p className={styles.eventDescription}>Мое описание</p>
                         <Link className={styles.eventLink} href='/'>
                             <span className={styles.eventLinkText}>Узнать подробнее</span>
